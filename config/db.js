a8e1226f96242affb9ab6c027832ae8f9b31bb88
David Carrillo <carrillod421@gmail.com>
2025-10-17 10:49:08 -0600

    act

--- config/db.js
+++ config/db.js
@@ -15,6 +15,6 @@ db.connect((err) => {
   console.log('Conectado a la base de datos MySQL');
 });
 
-//comentario de prueba del pipeline
+//comentario de prueba del pipelinezzzzzz
 
 module.exports = db;
